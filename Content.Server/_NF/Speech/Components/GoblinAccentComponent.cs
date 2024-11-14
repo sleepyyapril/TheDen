a33ec0995cebc08f2e4c408f8caffcebd1158ccf
sleepyyapril <flyingkarii@gmail.com>
2024-11-14 15:28:05 -0400

    Remove unused

--- Content.Server/_NF/Speech/Components/GoblinAccentComponent.cs
+++
@@ -1,8 +0,0 @@
-using Content.Server._NF.Speech.EntitySystems;
-
-namespace Content.Server._NF.Speech.Components;
-
-[RegisterComponent]
-[Access(typeof(GoblinAccentSystem))]
-public sealed partial class GoblinAccentComponent : Component
-{ }
--- Content.Server/_NF/Speech/Components/StreetpunkAccentComponent.cs
+++
@@ -1,7 +0,0 @@
-namespace Content.Server._NF.Speech.Components
-{
-    [RegisterComponent]
-    public sealed partial class StreetpunkAccentComponent : Component
-    {
-    }
-}
