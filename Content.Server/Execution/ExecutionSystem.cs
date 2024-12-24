7577f7d25afec4cbb69577d9339784eb76358253
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2024-12-23 21:18:41 -0400

    Update ExecutionSystem.cs

--- Content.Server/Execution/ExecutionSystem.cs
+++ Content.Server/Execution/ExecutionSystem.cs
@@ -5,7 +5,6 @@ using Content.Shared.ActionBlocker;
 using Content.Shared.Damage;
 using Content.Shared.Database;
 using Content.Shared.DoAfter;
-using Content.Shared.Execution;
 using Content.Shared.Interaction.Components;
 using Content.Shared.Mobs.Components;
 using Content.Shared.Mobs.Systems;
