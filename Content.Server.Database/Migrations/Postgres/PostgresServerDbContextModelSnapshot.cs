40210d850907f125281db8d93ff8196d3ca1f5cd
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-01-15 20:19:02 -0400

    Silicon Jobs: Name Customization (#158)
    <!--
    This is a semi-strict format, you can add/remove sections as needed but
    the order/format should be kept the same
    Remove these comments before submitting
    -->
    
    # Description
    
    <!--
    Explain this PR in as much detail as applicable
    
    Some example prompts to consider:
    How might this affect the game? The codebase?
    What might be some alternatives to this?
    How/Who does this benefit/hurt [the game/codebase]?
    -->
    
    Adds the ability to name your borgs based on your character.
    
    ---
    
    # Changelog
    
    <!--
    You can add an author after the `:cl:` to change the name that appears
    in the changelog (ex: `:cl: Death`)
    Leaving it blank will default to your GitHub display name
    This includes all available types for the changelog
    -->
    
    :cl:
    - add: Added cyborg and Station AI name customization.

--- Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
+++ Content.Server.Database/Migrations/Postgres/PostgresServerDbContextModelSnapshot.cs
@@ -913,6 +913,10 @@ namespace Content.Server.Database.Migrations.Postgres
                         .HasColumnType("text")
                         .HasColumnName("custom_specie_name");
 
+                    b.Property<string>("CyborgName")
+                        .HasColumnType("text")
+                        .HasColumnName("cyborg_name");
+
                     b.Property<string>("DisplayPronouns")
                         .HasColumnType("text")
                         .HasColumnName("display_pronouns");
@@ -990,6 +994,10 @@ namespace Content.Server.Database.Migrations.Postgres
                         .IsRequired()
                         .HasColumnType("text")
                         .HasColumnName("species");
+
+                    b.Property<string>("StationAiName")
+                        .HasColumnType("text")
+                        .HasColumnName("station_ai_name");
 
                     b.Property<float>("Width")
                         .HasColumnType("real")
