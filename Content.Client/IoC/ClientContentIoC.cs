abf5aac65abe1d0083d6e34fbc484ebc770848d7
sleepyyapril <flyingkarii@gmail.com>
2025-01-19 01:02:04 -0400

    !

--- Content.Client/IoC/ClientContentIoC.cs
+++ Content.Client/IoC/ClientContentIoC.cs
@@ -2,6 +2,7 @@ using Content.Client.Administration.Managers;
 using Content.Client.Changelog;
 using Content.Client.Chat.Managers;
 using Content.Client.Clickable;
+using Content.Client.Consent;
 using Content.Client.DeltaV.NanoChat;
 using Content.Client.DiscordAuth;
 using Content.Client.JoinQueue;
