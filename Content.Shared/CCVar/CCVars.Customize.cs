d43aea5d4427cefeb9d036338c867e2be3be6889
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-01-21 04:26:46 -0400

    Update CCVars.Customize.cs

--- Content.Shared/CCVar/CCVars.Customize.cs
+++ Content.Shared/CCVar/CCVars.Customize.cs
@@ -9,17 +9,17 @@ public sealed partial class CCVars
     ///     It looks something like "She also goes by they/them pronouns."
     /// </summary>
     public static readonly CVarDef<bool> AllowCosmeticPronouns =
-        CVarDef.Create("customize.allow_cosmetic_pronouns", false, CVar.REPLICATED);
+        CVarDef.Create("customize.allow_cosmetic_pronouns", true, CVar.REPLICATED);
 
     /// <summary>
     ///     Allow players to set their own Station AI names.
     /// </summary>
     public static readonly CVarDef<bool> AllowCustomStationAiName =
-        CVarDef.Create("customize.allow_custom_station_ai_name", false, CVar.REPLICATED);
+        CVarDef.Create("customize.allow_custom_station_ai_name", true, CVar.REPLICATED);
 
     /// <summary>
     ///     Allow players to set their own cyborg names. (borgs, mediborgs, etc)
     /// </summary>
     public static readonly CVarDef<bool> AllowCustomCyborgName =
-        CVarDef.Create("customize.allow_custom_cyborg_name", false, CVar.REPLICATED);
+        CVarDef.Create("customize.allow_custom_cyborg_name", true, CVar.REPLICATED);
 }
