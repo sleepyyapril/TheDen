1d946dcd58275cfb64dcb221cc276a2886ddf3a9
SlamBamActionman <83650252+SlamBamActionman@users.noreply.github.com>
2025-02-18 08:28:42 +0100

    Add ability for +VVEDIT users to scale damage/healing in the game (#35255)
    * Initial commit
    
    * Add universal modifier for all damage/heals, make guidebooks work.
    
    * help text

--- Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
+++ Content.Server/Damage/Systems/DamageOtherOnHitSystem.cs
@@ -68,6 +68,15 @@ namespace Content.Server.Damage.Systems
                 return;
             }
 
+            var dmg = _damageable.TryChangeDamage(args.Target, component.Damage * _damageable.UniversalThrownDamageModifier, component.IgnoreResistances, origin: args.Component.Thrower);
+
+            // Log damage only for mobs. Useful for when people throw spears at each other, but also avoids log-spam when explosions send glass shards flying.
+            if (dmg != null && HasComp<MobStateComponent>(args.Target))
+                _adminLogger.Add(LogType.ThrowHit, $"{ToPrettyString(args.Target):target} received {dmg.GetTotal():damage} damage from collision");
+
+            if (dmg is { Empty: false })
+                _color.RaiseEffect(Color.Red, [args.Target], Filter.Pvs(args.Target, entityManager: EntityManager));
+
             _stamina.TakeStaminaDamage(uid, damage.StaminaCost, component, visual: false);
         }
 
--- Content.Server/Weapons/Ranged/Systems/GunSystem.Cartridges.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.Cartridges.cs
@@ -36,7 +36,7 @@ public sealed partial class GunSystem
         if (damageSpec == null)
             return;
 
-        _damageExamine.AddDamageExamine(args.Message, damageSpec, Loc.GetString("damage-projectile"));
+        _damageExamine.AddDamageExamine(args.Message, Damageable.ApplyUniversalAllModifiers(damageSpec), Loc.GetString("damage-projectile"));
     }
 
     private DamageSpecifier? GetProjectileDamage(string proto)
@@ -51,7 +51,7 @@ public sealed partial class GunSystem
 
             if (!p.Damage.Empty)
             {
-                return p.Damage;
+                return p.Damage * Damageable.UniversalProjectileDamageModifier;
             }
         }
 
