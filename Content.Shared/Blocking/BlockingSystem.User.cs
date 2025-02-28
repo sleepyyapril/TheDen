a8ed506fb934c2f531a0ff9c48a72c01b0fddc97
TheGrimbeeper <thegrimbeeper.11@gmail.com>
2025-02-28 13:43:35 +1100

    Radiation protective armor and medicine now effect message frequency
    Hooked into the existing damage modifier system to allow detection of applicable radiation modifiers.
    
    Also:
    - if you can't take damage, you won't see the messages
    - adds a 'virtualDamage' field to the damage modify event to allow seeing what modifiers exist without actually dealing damage to things like shields.

--- Content.Shared/Blocking/BlockingSystem.User.cs
+++ Content.Shared/Blocking/BlockingSystem.User.cs
@@ -77,15 +77,19 @@ public sealed partial class BlockingSystem
 
         var blockFraction = blocking.IsBlocking ? blocking.ActiveBlockFraction : blocking.PassiveBlockFraction;
         blockFraction = Math.Clamp(blockFraction, 0, 1);
-        _damageable.TryChangeDamage(component.BlockingItem, blockFraction * args.OriginalDamage);
+        if (!args.IsVirtual) //#IMP Don't damage shield if we are just seeing what damage COULD be blocked
+            _damageable.TryChangeDamage(component.BlockingItem, blockFraction * args.OriginalDamage);
 
         var modify = new DamageModifierSet();
         foreach (var key in dmgComp.Damage.DamageDict.Keys)
             modify.Coefficients.TryAdd(key, 1 - blockFraction);
 
         args.Damage = DamageSpecifier.ApplyModifierSet(args.Damage, modify);
-        if (blocking.IsBlocking && !args.Damage.Equals(args.OriginalDamage))
+
+        if (blocking.IsBlocking && !args.Damage.Equals(args.OriginalDamage) && !args.IsVirtual) //#IMP => IsVirtual
+        {
             _audio.PlayPvs(blocking.BlockSound, uid);
+        }
     }
 
     private void OnDamageModified(EntityUid uid, BlockingComponent component, DamageModifyEvent args)
