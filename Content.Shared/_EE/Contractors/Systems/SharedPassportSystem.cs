6c5cdf8ef95a2f5b701db7ba1ac8a772ee1e9c9e
sleepyyapril <flyingkarii@gmail.com>
2025-04-03 19:46:41 -0300

    shitcode!

--- Content.Shared/_EE/Contractors/Systems/SharedPassportSystem.cs
+++ Content.Shared/_EE/Contractors/Systems/SharedPassportSystem.cs
@@ -86,10 +86,11 @@ public class SharedPassportSystem : EntitySystem
                 || !_storage.CanInsert(item.Value, passportEntity, out _, inventory, itemComp)
                 || !_storage.Insert(item.Value, passportEntity, out _, playSound: false))
             {
-                _entityManager.System<SharedAdminLogSystem>().Add(
+                /** _entityManager.System<SharedAdminLogSystem>().Add(
                     LogType.EntitySpawn,
                     LogImpact.Low,
                     $"Passport for {ev.Profile.Name} was spawned on the floor due to missing bag space");
+                    **/
             }
         }
     }
