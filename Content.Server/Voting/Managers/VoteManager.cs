9af9d278249a48bd310f3c59d52fe9b5a184c5d6
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-05-31 23:11:26 -0300

    Better Autocall (#626)
    Tested, blah blah blah.
    New admin command: togglehardend
    
    If used after hard end is reached, it allows shuttle to be recalled
    again.
    
    # TODO
    - [x] Round hard end implementation
    - [x] Server automated recall votes instead of votes controlled by
    command.
    
    :cl:
    - add: Round extension votes are now handled through an OOC vote, before
    the initial shuttle call is made.
    - add: Rounds will no longer last 6 hours and are now capped at 5,
    unless an admin intervenes beforehand.

--- Content.Server/Voting/Managers/VoteManager.cs
+++ Content.Server/Voting/Managers/VoteManager.cs
@@ -210,7 +210,7 @@ namespace Content.Server.Voting.Managers
             var start = _timing.RealTime;
             var end = start + options.Duration;
             var reg = new VoteReg(id, entries, options.Title, options.InitiatorText,
-                options.InitiatorPlayer, start, end);
+                options.InitiatorPlayer, start, end, options.PlayVoteSound);
 
             var handle = new VoteHandle(this, reg);
 
@@ -252,6 +252,7 @@ namespace Content.Server.Voting.Managers
                 msg.VoteInitiator = v.InitiatorText;
                 msg.StartTime = v.StartTime;
                 msg.EndTime = v.EndTime;
+                msg.PlayVoteSound = v.PlayVoteSound;
             }
 
             if (v.CastVotes.TryGetValue(player, out var cast))
@@ -445,6 +446,7 @@ namespace Content.Server.Voting.Managers
             public readonly HashSet<ICommonSession> VotesDirty = new();
 
             public bool Cancelled;
+            public bool PlayVoteSound;
             public bool Finished;
             public bool Dirty = true;
 
@@ -453,7 +455,7 @@ namespace Content.Server.Voting.Managers
             public ICommonSession? Initiator { get; }
 
             public VoteReg(int id, VoteEntry[] entries, string title, string initiatorText,
-                ICommonSession? initiator, TimeSpan start, TimeSpan end)
+                ICommonSession? initiator, TimeSpan start, TimeSpan end, bool playVoteSound)
             {
                 Id = id;
                 Entries = entries;
@@ -462,6 +464,7 @@ namespace Content.Server.Voting.Managers
                 Initiator = initiator;
                 StartTime = start;
                 EndTime = end;
+                PlayVoteSound = playVoteSound;
             }
         }
 
