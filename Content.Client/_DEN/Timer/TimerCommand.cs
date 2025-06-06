771e3324e73b6f88f3b5deb2e9fe69e52f314221
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-06-06 08:06:56 -0300

    Timers (#686)
    (timestringutilities is old code pls no bully)
    
    :cl:
    - add: Added a command to a set a timer for any length of time that does
    the vote ping sound effect when completed. The only caveat is that the
    timer does not last through rounds.

---
+++ Content.Client/_DEN/Timer/TimerCommand.cs
@@ -0,0 +1,50 @@
+using Content.Client.Atmos.EntitySystems;
+using Content.Shared._DEN.Timestring;
+using Content.Shared.Atmos;
+using Content.Shared.GameTicking;
+using JetBrains.Annotations;
+using Robust.Shared.Console;
+
+namespace Content.Client._DEN.Timer;
+
+[UsedImplicitly]
+internal sealed class TimerCommand : LocalizedCommands
+{
+    [Dependency] private readonly IEntitySystemManager _sysManager = default!;
+
+    public override string Command => "timer";
+
+    public override string Help => LocalizationManager.GetString($"cmd-{Command}-help", ("command", Command));
+
+    public override void Execute(IConsoleShell shell, string argStr, string[] args)
+    {
+        if (args.Length != 1)
+        {
+            var argsError = LocalizationManager.GetString($"cmd-{Command}-args-error", ("command", Command));
+            shell.WriteError(argsError);
+            return;
+        }
+
+        var minutes = TimestringUtilities.CountMinutes(args[0]);
+        var timerSystem = _sysManager.GetEntitySystem<TimerSystem>();
+        var gameTicker = _sysManager.GetEntitySystem<SharedGameTicker>();
+        var curTime = gameTicker.RoundDuration();
+        var runAt = curTime + TimeSpan.FromMinutes(minutes);
+        var success = LocalizationManager.GetString(
+            $"cmd-{Command}-success",
+                ("command", Command),
+                ("run-at", runAt.ToString(@"hh\:mm\:ss"))
+            );
+
+        timerSystem.AddSimpleTimer(runAt);
+        shell.WriteLine(success);
+    }
+
+    public override CompletionResult GetCompletion(IConsoleShell shell, string[] args)
+    {
+        if (args.Length == 1)
+            return CompletionResult.FromHint("<timestring (e.g. 4h23m)>");
+
+        return CompletionResult.Empty;
+    }
+}
---
+++ Resources/Locale/en-US/_DEN/commands/timer-command.ftl
@@ -0,0 +1,4 @@
+cmd-timer-help = {$command} <timestring (e.g. 4h23m)> 
+cmd-timer-desc = Set a timer with a timestring.
+cmd-timer-args-error = Please input a timestring, such as 4h23m for 4 hours and 23 minutes.
+cmd-timer-success = Successfully set a timer for {$run-at}
