fb12d9ba2804ed06c21cfecc8c0050c2db8b0101
sleepyyapril <flyingkarii@gmail.com>
2025-06-14 13:38:22 -0300

    !

--- Content.Server/StationEvents/Events/RandomSpawnRule.cs
+++ Content.Server/StationEvents/Events/RandomSpawnRule.cs
@@ -1,3 +1,4 @@
+using Content.Server._Lavaland.Procedural.Components;
 using Content.Server.GameTicking.Rules.Components;
 using Content.Server.StationEvents.Components;
 using Content.Shared.GameTicking.Components;
@@ -6,13 +7,15 @@ namespace Content.Server.StationEvents.Events;
 
 public sealed class RandomSpawnRule : StationEventSystem<RandomSpawnRuleComponent>
 {
+    private bool Filter(EntityUid map) => !HasComp<LavalandMapComponent>(map);
+
     protected override void Started(EntityUid uid, RandomSpawnRuleComponent comp, GameRuleComponent gameRule, GameRuleStartedEvent args)
     {
         base.Started(uid, comp, gameRule, args);
 
         if (comp.StationExclusive)
         {
-            var found = TryGetRandomStationData(out var station);
+            var found = TryGetRandomStationData(out var station, filter: Filter);
 
             if (
                 found &&
