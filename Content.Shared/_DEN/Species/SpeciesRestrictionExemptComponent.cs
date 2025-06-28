d561bb20b95ffce83ccf7c2d1a0dc516699df672
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-06-28 07:13:30 -0300

    !

--- Content.Shared/_DEN/Species/SpeciesRestrictionExemptComponent.cs
+++ Content.Shared/_DEN/Species/SpeciesRestrictionExemptComponent.cs
@@ -2,7 +2,7 @@ namespace Content.Shared._DEN.Species;
 
 
 /// <summary>
-/// This is used for...
+/// This is used for making a user exempt to species height/width restrictions.
 /// </summary>
 [RegisterComponent]
 public sealed partial class SpeciesRestrictionExemptComponent : Component;
