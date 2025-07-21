b96cc1b94295bdbb4607aabdf49223092b37a12a
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-07-21 06:59:48 -0300

    migration

---
+++ Content.Server.Database/Migrations/Postgres/20250721095544_NsfwPopupDisclaimer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class NsfwPopupDisclaimer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "accepted_prompt",
+                table: "player",
+                type: "boolean",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "accepted_prompt",
+                table: "player");
+        }
+    }
+}
---
+++ Content.Server.Database/Migrations/Sqlite/20250721095531_NsfwPopupDisclaimer.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class NsfwPopupDisclaimer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "accepted_prompt",
+                table: "player",
+                type: "INTEGER",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "accepted_prompt",
+                table: "player");
+        }
+    }
+}
