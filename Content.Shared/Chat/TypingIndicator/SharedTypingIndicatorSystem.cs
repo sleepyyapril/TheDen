4b71705fffbf3b3715d29d35153fa85db94e652d
Jakumba <16194688+Jakumba@users.noreply.github.com>
2025-08-01 13:00:21 +0100

    Merge cleanup

--- Content.Shared/Chat/TypingIndicator/SharedTypingIndicatorSystem.cs
+++ Content.Shared/Chat/TypingIndicator/SharedTypingIndicatorSystem.cs
@@ -1,33 +1,38 @@
-// SPDX-FileCopyrightText: 2022 Alex Evgrashin <aevgrashin@yandex.ru>
-// SPDX-FileCopyrightText: 2023 Morb <14136326+Morb0@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2023 Vordenburg <114301317+Vordenburg@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2024 DEATHB4DEFEAT <77995199+DEATHB4DEFEAT@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 88tv <131759102+88tv@users.noreply.github.com>
-// SPDX-FileCopyrightText: 2025 lzk <124214523+lzk228@users.noreply.github.com>
-//
-// SPDX-License-Identifier: AGPL-3.0-or-later AND MIT
-
+using Content.Shared.ActionBlocker;
 using Content.Shared.Clothing;
+using Content.Shared.Inventory;
+using Content.Shared.Chat.TypingIndicator;
+using Robust.Shared.Player;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Timing;
 
 namespace Content.Shared.Chat.TypingIndicator;
 
 /// <summary>
-///     Sync typing indicator icon between client and server.
+///     Supports typing indicators on entities.
 /// </summary>
 public abstract class SharedTypingIndicatorSystem : EntitySystem
 {
+    [Dependency] private readonly ActionBlockerSystem _actionBlocker = default!;
+    [Dependency] private readonly SharedAppearanceSystem _appearance = default!;
+    [Dependency] private readonly IGameTiming _timing = default!;
+
     /// <summary>
     ///     Default ID of <see cref="TypingIndicatorPrototype"/>
     /// </summary>
-    [ValidatePrototypeId<TypingIndicatorPrototype>]
-    public const string InitialIndicatorId = "default";
+    public static readonly ProtoId<TypingIndicatorPrototype> InitialIndicatorId = "default";
 
     public override void Initialize()
     {
         base.Initialize();
+        SubscribeLocalEvent<PlayerAttachedEvent>(OnPlayerAttached);
+        SubscribeLocalEvent<TypingIndicatorComponent, PlayerDetachedEvent>(OnPlayerDetached);
+
         SubscribeLocalEvent<TypingIndicatorClothingComponent, ClothingGotEquippedEvent>(OnGotEquipped);
         SubscribeLocalEvent<TypingIndicatorClothingComponent, ClothingGotUnequippedEvent>(OnGotUnequipped);
+        SubscribeLocalEvent<TypingIndicatorClothingComponent, InventoryRelayedEvent<BeforeShowTypingIndicatorEvent>>(BeforeShow);
+
+        SubscribeAllEvent<TypingChangedEvent>(OnTypingChanged);
     }
 
     private void OnPlayerAttached(PlayerAttachedEvent ev)
@@ -38,6 +43,7 @@ public abstract class SharedTypingIndicatorSystem : EntitySystem
         EnsureComp<AppearanceComponent>(ev.Entity);
     }
 
+
     private void OnPlayerDetached(EntityUid uid, TypingIndicatorComponent component, PlayerDetachedEvent args)
     {
         // player left entity body - hide typing indicator
@@ -46,7 +52,25 @@ public abstract class SharedTypingIndicatorSystem : EntitySystem
 
     private void OnGotEquipped(Entity<TypingIndicatorClothingComponent> entity, ref ClothingGotEquippedEvent args)
     {
-        if (!TryComp<TypingIndicatorComponent>(args.Wearer, out var indicator))
+        entity.Comp.GotEquippedTime = _timing.CurTime;
+    }
+
+    private void OnGotUnequipped(Entity<TypingIndicatorClothingComponent> entity, ref ClothingGotUnequippedEvent args)
+    {
+        entity.Comp.GotEquippedTime = null;
+    }
+
+    private void BeforeShow(Entity<TypingIndicatorClothingComponent> entity, ref InventoryRelayedEvent<BeforeShowTypingIndicatorEvent> args)
+    {
+        args.Args.TryUpdateTimeAndIndicator(entity.Comp.TypingIndicatorPrototype, entity.Comp.GotEquippedTime);
+    }
+
+    private void OnTypingChanged(TypingChangedEvent ev, EntitySessionEventArgs args)
+    {
+        var uid = args.SenderSession.AttachedEntity;
+        if (!Exists(uid))
+        {
+            Log.Warning($"Client {args.SenderSession} sent TypingChangedEvent without an attached entity.");
             return;
         }
 
