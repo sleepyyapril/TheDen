b172162b99c3c5437c6a4fa6110dd95eb38e3216
Sir Warock <67167466+SirWarock@users.noreply.github.com>
2025-08-03 10:08:20 +0200

    Fix YAML fail

--- Content.Shared/Item/ItemToggle/Components/ItemToggleComponent.cs
+++ Content.Shared/Item/ItemToggle/Components/ItemToggleComponent.cs
@@ -42,6 +42,18 @@ public sealed partial class ItemToggleComponent : Component
     public bool OnUse = true;
 
     /// <summary>
+    ///     The localized text to display in the verb to activate.
+    /// </summary>
+    [DataField]
+    public string VerbToggleOn = "item-toggle-activate";
+
+    /// <summary>
+    ///     The localized text to display in the verb to de-activate.
+    /// </summary>
+    [DataField]
+    public string VerbToggleOff = "item-toggle-deactivate";
+
+    /// <summary>
     /// Goobstation
     /// Don't toggle on wielding/unwielding if false
     /// </summary>
