037f3cf379c9c7055a46c601cb67c65234ad0f49
FoxxoTrystan <45297731+FoxxoTrystan@users.noreply.github.com>
2025-09-17 18:09:40 +0200

    Merge pull request #1234 from RHSvenson/FormsAndFields
    Forms and fields

--- Content.Shared/Paper/PaperComponent.cs
+++ Content.Shared/Paper/PaperComponent.cs
@@ -76,6 +76,18 @@ public sealed partial class PaperComponent : Component
         }
     }
 
+    // Starlight-start
+    [Serializable, NetSerializable]
+    public sealed class PaperSignatureRequestMessage : BoundUserInterfaceMessage
+    {
+        public readonly int SignatureIndex;
+
+        public PaperSignatureRequestMessage(int signatureIndex)
+        {
+            SignatureIndex = signatureIndex;
+        }
+    }
+    // Starlight-end
     [Serializable, NetSerializable]
     public enum PaperUiKey
     {
