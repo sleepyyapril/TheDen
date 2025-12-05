c008510697b1ed51403c33844c1d9dfc774a1e98
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2025-12-05 02:21:48 -0400

    Minor Logistics Trolling (#2146)
    This changes:
    - Diamond sell price to 10, instead of 20.
    - Bounties can be rerolled every 30 second. Please don't abuse this;
    it's so others don't NEED to do your bounties if they don't want to.
    - Halved mining points from Diamond and Bananium
    - Spesos from salvage vendor is 2:1, instead of 1:1 mining points to
    spesos.
    
    :cl:
    - tweak: Diamond sell price is 10 instead of 20.
    - tweak: Bounties can be rerolled every 30 seconds. Please don't abuse
    this; it's so others don't NEED to do your bounties if they don't want
    to.
    - tweak: Halved mining points from Diamond and Bananium
    - tweak: Spesos from salvage vendor is 2:1, instead of 1:1 mining points
    to spesos.

--- Content.Server/Cargo/Components/StationCargoBountyDatabaseComponent.cs
+++ Content.Server/Cargo/Components/StationCargoBountyDatabaseComponent.cs
@@ -60,5 +60,5 @@ public sealed partial class StationCargoBountyDatabaseComponent : Component
     /// The time between skipping bounties.
     /// </summary>
     [DataField]
-    public TimeSpan SkipDelay = TimeSpan.FromMinutes(15);
+    public TimeSpan SkipDelay = TimeSpan.FromSeconds(30);
 }
