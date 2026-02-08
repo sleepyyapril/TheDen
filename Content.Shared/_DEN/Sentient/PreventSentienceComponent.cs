5bbab28b0a1edb0b6dce03a7daf92d5233ab3e5d
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2026-02-07 23:29:56 -0400

    Feat: Prevent Dungeon Dragon Sentience (#2391)
    prevents a salvage dragon from gaining sentience via cognizine

---
+++ Content.Shared/_DEN/Sentient/PreventSentienceComponent.cs
@@ -0,0 +1,8 @@
+namespace Content.Shared._DEN.Sentient;
+
+
+/// <summary>
+/// This is used for preventing sentience on a mob.
+/// </summary>
+[RegisterComponent]
+public sealed partial class PreventSentienceComponent : Component;
