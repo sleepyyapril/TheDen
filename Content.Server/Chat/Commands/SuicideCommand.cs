ae6e7163359c1ec5afd46fa0e414a1c9a6c852ab
sleepyyapril <123355664+sleepyyapril@users.noreply.github.com>
2026-02-09 02:28:07 -0400

    Remove: Suicide Command (#2400)
    no cl.

--- Content.Server/Chat/Commands/SuicideCommand.cs
+++ Content.Server/Chat/Commands/SuicideCommand.cs
@@ -46,56 +46,55 @@ using Content.Shared.Mind;
 using Robust.Shared.Console;
 using Robust.Shared.Enums;
 
-namespace Content.Server.Chat.Commands
+namespace Content.Server.Chat.Commands;
+/*
+[AnyCommand]
+internal sealed class SuicideCommand : IConsoleCommand
 {
-    [AnyCommand]
-    internal sealed class SuicideCommand : IConsoleCommand
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+
+    public string Command => "suicide";
+
+    public string Description => Loc.GetString("suicide-command-description");
+
+    public string Help => Loc.GetString("suicide-command-help-text");
+
+    public void Execute(IConsoleShell shell, string argStr, string[] args)
     {
-        [Dependency] private readonly IEntityManager _entityManager = default!;
-
-        public string Command => "suicide";
-
-        public string Description => Loc.GetString("suicide-command-description");
-
-        public string Help => Loc.GetString("suicide-command-help-text");
-
-        public void Execute(IConsoleShell shell, string argStr, string[] args)
+        if (shell.Player is not { } player)
         {
-            if (shell.Player is not { } player)
-            {
-                shell.WriteError(Loc.GetString("shell-cannot-run-command-from-server"));
-                return;
-            }
-
-            if (player.Status != SessionStatus.InGame || player.AttachedEntity == null)
-                return;
-
-            var minds = _entityManager.System<SharedMindSystem>();
-
-            // This check also proves mind not-null for at the end when the mob is ghosted.
-            if (!minds.TryGetMind(player, out var mindId, out var mindComp) ||
-                mindComp.OwnedEntity is not { Valid: true } victim)
-            {
-                shell.WriteLine(Loc.GetString("suicide-command-no-mind"));
-                return;
-            }
-
-            var suicideSystem = _entityManager.System<SuicideSystem>();
-
-            if (_entityManager.HasComponent<AdminFrozenComponent>(victim))
-            {
-                var deniedMessage = Loc.GetString("suicide-command-denied");
-                shell.WriteLine(deniedMessage);
-                _entityManager.System<PopupSystem>()
-                    .PopupEntity(deniedMessage, victim, victim);
-                return;
-            }
-
-            if (suicideSystem.Suicide(victim))
-                return;
-
-            shell.WriteLine(Loc.GetString("ghost-command-denied"));
-            shell.WriteLine(Loc.GetString("ghost-command-denied"));
+            shell.WriteError(Loc.GetString("shell-cannot-run-command-from-server"));
+            return;
         }
+
+        if (player.Status != SessionStatus.InGame || player.AttachedEntity == null)
+            return;
+
+        var minds = _entityManager.System<SharedMindSystem>();
+
+        // This check also proves mind not-null for at the end when the mob is ghosted.
+        if (!minds.TryGetMind(player, out var mindId, out var mindComp) ||
+            mindComp.OwnedEntity is not { Valid: true } victim)
+        {
+            shell.WriteLine(Loc.GetString("suicide-command-no-mind"));
+            return;
+        }
+
+        var suicideSystem = _entityManager.System<SuicideSystem>();
+
+        if (_entityManager.HasComponent<AdminFrozenComponent>(victim))
+        {
+            var deniedMessage = Loc.GetString("suicide-command-denied");
+            shell.WriteLine(deniedMessage);
+            _entityManager.System<PopupSystem>()
+                .PopupEntity(deniedMessage, victim, victim);
+            return;
+        }
+
+        if (suicideSystem.Suicide(victim))
+            return;
+
+        shell.WriteLine(Loc.GetString("ghost-command-denied"));
     }
 }
+*/
